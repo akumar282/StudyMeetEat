164a2293a5b36b6c68ee1ab3b0ed903a7e9c2a01
Hayden Lauritzen <haydenlauritzen@gmail.com>
2023-05-06 19:31:47 -0700

    create meet

--- tests/backend/mutations/userMutation.test.ts
+++ tests/backend/mutations/userMutation.test.ts
@@ -2,27 +2,13 @@ import { Amplify } from 'aws-amplify'
 import { CreateUsersModelInput } from '../../../src/API'
 import { createUser, deleteUser, updateUser } from '../../../src/backend/mutations/userMutations'
 import awsconfig from '../../../src/aws-exports'
+import { generateID, generateUUID } from '../../backend/utils'
+
 Amplify.configure(awsconfig)
-
-function generateUUID() {
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'
-  .replace(/[xy]/g, function(c) {
-    var r = Math.random() * 16 | 0, v = c === 'x' ? r : (r & 0x3 | 0x8)
-    return v.toString(16)
-  });
-}
-
-function generateID() {
-
-  const min = 0 // Minimum 4-digit number
-  const max = 999999 // Maximum 4-digit number
-
-  return Math.floor(Math.random() * (max - min + 1)) + min
-}
 
 describe('User Mutations', () => {
 
-  test('Create User', async () => {
+  test.skip('Create User', async () => {
 
     let idNum = generateID()
 
